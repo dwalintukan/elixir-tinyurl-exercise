3a3cfbfb92e50202942b4ae4c7879c7ebd3faf59
Deric Walintukan <deric@loopapps.com>
2021-02-22 13:44:03 -0800

    Add encode link func

--- tinyurl/lib/tinyurl/service.ex
+++ tinyurl/lib/tinyurl/service.ex
@@ -9,4 +9,7 @@ defmodule TinyUrl.Service do
   def fetch_original_link() do
 
   end
+
+  # Not a real hashing solution! Just temporary for this exercise.
+  defp encode_original_link(link), do: :crypto.hash(:sha256, link) |> Base.encode16()
 end
